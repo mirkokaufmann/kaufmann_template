2fa7664392606b7502df9d034782d354828865cb
Mirko Kaufmann <info@internetagentur-kaufmann.de>
2012-12-18 18:36:08 +0100

    Fixed some wrong paths.

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'KAUFMANN Website Template');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/', 'KAUFMANN Website Template');
 
 ?>
